110faea9b52fccaebbc9076270ba059e45795181
Bob Kupperstein <bkupperstein@datto.com>
2019-05-15 17:03:39 -0400

    first step mavenizing

--- src/JsonConverter.java
+++
@@ -1,6 +0,0 @@
-//import org.codehaus.jackson.JsonGenerationException;
-//import org.codehaus.jackson.map.JsonMappingException;
-//import org.codehaus.jackson.map.ObjectMapper;
-
-public class JsonConverter {
-}
